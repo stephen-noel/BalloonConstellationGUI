18007826ac5deb33829d9ca0acc5025033d4379a
sregmi32 <sregmi@vt.edu>
2017-07-19 08:34:30 -0400

    Update convertWind.m

--- trajectory/convertWind.m
+++ trajectory/convertWind.m
@@ -3,7 +3,12 @@
 % inputs are user inputs
 % outputs are what x(aka u) y(aka v) wind speeds NOAA gives us
 
-function [ugrd_3658m, vgrd_3658m] = convertWind(inputlat1,inputlong2)
+function [ugrd_3658m, vgrd_3658m] = convertWind(inputlat1,inputlong1)
+scenarioStartTime = datetime(d,'InputFormat','YYYYMMDD')
+windspeed = sqrt((ugrd_3658m)^2+(vgrd_3658m)^2)
+inputlat1 = 
+inputlong1 = 
+
 
 
 % NOTE: inputs and outputs are placeholders
