1c81b142593af2155ddf4ade3f69ecc3800ecaa5
sregmi32 <sregmi@vt.edu>
2017-07-24 08:25:54 -0400

    Update convertWind.m

--- trajectory/convertWind.m
+++ trajectory/convertWind.m
@@ -7,9 +7,9 @@ function [time, latitude, longitude] = convertWind(scenarioStartTime, launchLat,
 
 load WindData_NOMADS_GFS;
 
-WindData_NOMADS_GFS = table(ugrd_3658m, vgrd_3658m,'RowNames',XY);
-summary(WindData_NOMADS_GFS);
-T1 = WindData_NOMADS_GFS(1:2,:);
+WindData = table(ugrd_3658m, vgrd_3658m,'RowNames',XY);
+summary(WindData);
+T1 = WindData(1:2,:);
 
 windspeed = sqrt((ugrd_3658m)^2+(vgrd_3658m)^2); %m/s
 direction = atan2(norm(cross(ugrd_3658m,vgrd_3658m)),dot(ugrd_3658m,vgrd_3658m)); %in degrees
@@ -17,10 +17,11 @@ direction = atan2(norm(cross(ugrd_3658m,vgrd_3658m)),dot(ugrd_3658m,vgrd_3658m))
 
 
 for time = 0:1:10800 %%in seconds
-  dx = vgrd_3658m * time;
-  dy = ugrd_3658m * time;
-  latitude = dx + launchLat;
- longitude = dy + launchLon;
+
+latitude(t) = launchLat + uvel.*time;
+longitude(t) = launchLon + vvel.*time;
+
+
 end
 
 
