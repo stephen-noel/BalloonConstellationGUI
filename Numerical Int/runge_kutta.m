29ff571c57762331a67a16da4c1b57f983113965
Kellie Matthews <kelliem@vt.edu>
2017-07-26 14:31:54 -0400

    Update runge_kutta.m
    Run function to get plot. Still need to figure out how to get the variables to update within the function starting at line 52. For now the function just uses initial variables.

--- Numerical Int/runge_kutta.m
+++ Numerical Int/runge_kutta.m
@@ -1,20 +1,4 @@
-% http://12000.org/my_notes/mma_matlab_control/KERNEL/KEse159.htm#x164-2050004.22
-
-% Let z = z(t), z'(t) = v(t) and v'(t)=z"(t)=f(z,v,t)
-
-% Equation:
-% z"(t)=(g*(RhoA*vol-mass)-.5*RhoA*realCD*z'(t)*abs(z'(t))*Ca)/(mass+cb*RhoA*vol)
-%mass = PMass+BMass+Mb
-
-% f = @(z,v,t) (g*(RhoA*vol-mass)-.5*RhoA*realCD*v(t)*abs(v(t))*Ca)/(mass+cb*RhoA*vol)
-
-% h: time step
-% t: total time
-% z0: initial altitude
-% v0: initial velocity
-
-clear;
-clc;
+function [z,v,t] = rktest( )
 
 realCD = .47;
 BMass = 12; %kg
@@ -36,72 +20,39 @@ cb = 0.55; % This is the apparent additional mass coefficient for balloons based
 g = 9.81; % acceleration caused by gravity (m/sec^2)
 dt = 1;
 radius = ((3/(4*pi))*vol)^(1/3);
-
-
-t = 10800;
-%Initial conditions
-z0 = 1;
-v0 = 0;
-z(1) = z0;
-v(1) = v0;
-h = 1;
-
-N = t - 1;
+Ca = pi*radius.^2;
 mass = PMass+BMass+Mb;
-for i = 1:N
-    
-         oldTemp = temp;
-    oldTempK = tempK; % https://www.grc.nasa.gov/www/k-12/airplane/atmosmet.html
-    if (z <= 11000) %Meters (Troposhpere)
-        temp = 15.04 - 0.00649*z; 
-        tempK = temp + 273.15;
-        p = 101.29*((temp+273.1)/(288.08)).^5.256; %kPa
-        
-    elseif (z > 11000 && z < 25000) %Meters (Lower Stratosphere)
-        temp = -56.46;
-        tempK = temp + 273.15;
-        p = 22.65*exp(1.73-0.000157*z); %kPa
-        
-    else %Upper Stratosphere
-        temp = -131.21 + 0.00299*z; 
-        tempK = temp + 273.15;
-        p = 2.488 * ((temp+273.1)/216.6).^-11.388; %kPa
-    end
 
-    dTemp = abs(temp - oldTemp);
-    dTempK = abs(tempK - oldTempK); 
-    
-    RhoA = (p/(.2869*tempK))*9.81; %This is the density of air which needs to be updated. [kg/m^2s^2] Newtons per meter.
-                                   % RhoA needs to be in Newtons per meter for the calculation in C
-    Wg = Mb.*(1000*p).*vol/(r.*tempK); %The weight of the gas has to be updated for the new temperature
-    
-    radius = ((3/(4*pi))*vol).^(1/3);
-    Ca = pi*radius.^2;
-    
-    f = @(z,v,t) (g*(RhoA*vol-mass)-.5*RhoA*realCD*v(i)*abs(v(i))*Ca)/(mass+cb*RhoA*vol);
-    
+h = 1;
+t = 0:200;
+N = length(t);
+z = zeros(N,1);
+v = zeros(N,1);
+z(1) = 1;
+v(1) = 0;
+
+for i = 1:(N-1)   
     
     k1 = h * v(i);
-    s1 = h * f(t(i), z(i), v(i));
+    s1 = h * f(v(i));
     
     k2 = h * (v(i) + 1/2* s1);
-    s2 = h * f(t(i)+1/2*h , z(i)+1/2*k1 , v(i)+1/2*s1);
+    s2 = h * f(v(i)+1/2*s1);
     
     k3 = h * (v(i) + 1/2* s2);
-    s3 = h * f(t(i)+1/2*h , z(i)+1/2*k2 , v(i)+1/2*s2);
+    s3 = h * f(v(i)+1/2*s2);
     
     k4 = h * (v(i) + s3);
-    s4 = h * f(t(i)+h , z(i)+k3 , v(i)+s3);
+    s4 = h * f(v(i)+s3);
     
     z(i+1) = z(i)+1/6*(k1+2*k2+2*k3+k4);
     
     v(i+1) = v(i)+1/6*(s1+2*s2+2*s3+s4);
-
 end
-
-t = 1:length(z);
-scatter(t,z,'filled');
-
-title('Altitude versus Time')
-xlabel('Time [seconds]')
-ylabel('Altitude [meters]')
+    function r=f(v)
+        
+        r=(g*(RhoA*vol-mass)-.5*RhoA*realCD*v*abs(v)*Ca)/(mass+cb*RhoA*vol);
+        
+    end
+plot(t,z)
+end
