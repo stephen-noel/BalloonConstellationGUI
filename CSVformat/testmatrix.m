cb4b457714185d630ba390b6b98389ade51c7792
sregmi32 <sregmi@vt.edu>
2017-08-07 09:45:13 -0400

    Update testmatrix.m

--- CSVformat/testmatrix.m
+++ CSVformat/testmatrix.m
@@ -1,29 +1,17 @@
 %% Test Matrix Data
 
-
-%% Matrix instantiation
 %length of arrays
-idx_end = 3*60; %3hrs in seconds
-epSec = 10;
-oldDateSTR = '02 Aug 2017 16:00:00.000';
+idx_end = 3*60*60; %3hrs in seconds
 
-%loop to create datetime strings 
-for i = 1:idx_end;
-[newDateSTR] = epSec2Time(epSec, oldDateSTR);
-time_array{i} = newDateSTR;
-oldDateSTR = newDateSTR;
-end
-
-%position arrays
-alt_array = num2cell(3*rand(length(time_array),1));
-lat_array = num2cell(4*rand(length(time_array),1));
-lon_array = num2cell(5*rand(length(time_array),1));
-time_array = transpose(time_array);
+%separate time and position arrays
+time_array = transpose(1:idx_end);
+alt_array = 3*rand(length(time_array),1);
+lat_array = 4*rand(length(time_array),1);
+lon_array = 5*rand(length(time_array),1);
 
 %test matrix
 test_matrix = horzcat(time_array, alt_array, lat_array, lon_array);
-
-%% CSV writing
+elasped_time = datetimeVAR;
 
 %CSV writing method
 csvwrite('csvlist.dat',test_matrix) 
@@ -33,10 +21,18 @@ csvwrite('csvlist.csv',test_matrix) %where test_matrix is matrix output by GUI
 type csvlist.csv
 
 %user input to save as an Excel filename of their choice
+
 xlsFileName = 'CSV'; %the file will save as CSV.xls
-xlswrite(xlsFileName, test_matrix,'Sheet1','A2');
+xlswrite(xlsFileName,test_matrix,'Sheet1','B2');
+xlswrite(xlsFileName,elapsed_time,'Sheet1','A2');
 col_header={'Elapsed Time [s]','Altitude [m]','Latitude [deg]','Longitude [deg]','','','','','','',''};
 xlswrite('CSV.xls',col_header,'Sheet1'); %write column 1 header
+
+
+%column should be dates, STK dates in strings
+%data type conversion
+%first column: they're all strings, she made them into cells (columns 2,3,4
+%are cells)
 
 
 
