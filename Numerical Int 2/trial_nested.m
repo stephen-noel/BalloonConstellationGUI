4a2ea8139fd51fc1653e8386e4b5ed002ebd5835
Julianna Evans <julevans@vt.edu>
2017-08-08 15:24:59 -0400

    Added scripts and folder "numerical int 2" to help with debugging

---
+++ Numerical Int 2/trial_nested.m
@@ -0,0 +1,8 @@
+clear
+clc
+dz=0;
+vol=4;
+tempK = 288;
+
+[t,x]=ode45('nestedfunctions',[0,15000],[1,0]);
+%plot(t,x(:,1))
---
+++ Numerical Int 2/volume.m
@@ -0,0 +1,5 @@
+function dVol = volume(t,x,p,Wg,dTempK,RhoA,vol,dz)
+    dt = 1;
+    dVol = (8.314/(p*MW))*((Wg*dTempK/dt)*dt + (RhoA/p)*(vol)*dz);
+
+end
