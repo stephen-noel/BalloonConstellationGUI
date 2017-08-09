9099f835e3f1f8387fc3004cb31722bc89a8f5b9
Kellie Matthews <kelliem@vt.edu>
2017-08-09 11:43:01 -0400

    Update volume.m

--- Numerical Int 2/volume.m
+++ Numerical Int 2/volume.m
@@ -1,4 +1,4 @@
-function dVol = volume(t,x,p,Wg,dTempK,RhoA,vol,dz)
+function dVol = volume(t,x,p,Wg,dTempK,RhoA,vol,dz,MW)
     dt = 1;
     dVol = (8.314/(p*MW))*((Wg*dTempK/dt)*dt + (RhoA/p)*(vol)*dz);
 
